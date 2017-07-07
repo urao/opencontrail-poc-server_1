00d287c44627c4f04c2005ffeb6b40ba85470d21
urao <urao@juniper.net>
2017-07-06 22:46:59 -0700

    modified this file

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-echo 'Running the complete deployment scripts from here'
-./setup_jumphost.sh && echo "Setup Jumphost done, Running ServerManager Provisioning" && ./smgr_provision.sh && echo "Provisioning ServerManager done, Running Contrail Provisioning" && ./contrail_provision.sh && echo "Provisioning Contrail done, Running Post deployment" && ./post_deployment.sh && echo "Post deployment done, Complete Contrail deployment, SUCCESSFULLY!!!!"
+echo -e '---Running the complete deployment scripts from here---\n'
+echo -e '---(1)Running the jumphost setup---\n'
+./setup_jumphost.sh >/dev/null && echo -e "---(2)Setup Jumphost done, Running ServerManager Provisioning---\n" && ./smgr_provision.sh && echo -e "---(3)Provisioning ServerManager done, Running Contrail Provisioning---\n" && ./contrail_provision.sh && echo -e "---(4)Provisioning Contrail done, Running Post deployment---\n" && ./post_deployment.sh && echo -e "---(5)Post deployment done, Complete Contrail deployment, SUCCESSFULLY!!!!---\n"
